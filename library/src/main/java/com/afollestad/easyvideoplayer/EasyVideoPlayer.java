af4bec53a427fa512a81c21737b1f22b53c23ff4
Francisco Franco <franciscofranco.1990@gmail.com>
2016-06-24 02:34:11 +0100

    fullScreenListener is not defined, fix my copy pasta derp
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- library/src/main/java/com/afollestad/easyvideoplayer/EasyVideoPlayer.java
+++ library/src/main/java/com/afollestad/easyvideoplayer/EasyVideoPlayer.java
@@ -934,7 +934,6 @@ public class EasyVideoPlayer extends FrameLayout implements IUserMethods, Textur
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
             if (mAutoFullscreen) {
                 setSystemUiVisibility(fullscreen ? fullscreenFlags() : View.SYSTEM_UI_FLAG_VISIBLE);
-                setOnSystemUiVisibilityChangeListener(fullScreenListener);
             }
         }
     }
